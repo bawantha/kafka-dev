2ba0e90a4792d0abe4a1b3b117dc4ca3a3363556
bawantha <bawwa.16@cse.mrt.ac.lk>
2020-06-18 03:29:32 +0530

    Initial commit

---
+++ src/main/java/com/github/bawantha/kafka/tuto1/ProducerDemo.java
@@ -0,0 +1,24 @@
+package com.github.bawantha.kafka.tuto1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+
+    public static void main(String[] args) {
+        String bootstrapServer = "127.0.0.1:9092";
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+
+    }
+}
